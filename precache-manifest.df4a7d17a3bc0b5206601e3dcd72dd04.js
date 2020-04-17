7d83dbddcf0f94daa353057b83439aedcf6818f5
vishal-raj15 <vishalraj1380@gmail.com>
2020-04-18 03:59:42 +0530

    Updates

---
+++ precache-manifest.df4a7d17a3bc0b5206601e3dcd72dd04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f2dcd5540feab49fbf37ed912ad1eee",
+    "url": "/youtube_prototype/index.html"
+  },
+  {
+    "revision": "e1e44ac449017fa60c92",
+    "url": "/youtube_prototype/static/css/main.b8775964.chunk.css"
+  },
+  {
+    "revision": "819fca532c6d573d4d44",
+    "url": "/youtube_prototype/static/js/2.c10682e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube_prototype/static/js/2.c10682e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1e44ac449017fa60c92",
+    "url": "/youtube_prototype/static/js/main.4e6c24e0.chunk.js"
+  },
+  {
+    "revision": "71ab9975f1f66a95b94a",
+    "url": "/youtube_prototype/static/js/runtime-main.fdd61822.js"
+  }
+]);
